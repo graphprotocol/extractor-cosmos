8948b10815e112822e497d8726a2466f8e987880
Łukasz Miłkowski <lukanus@uaznia.net>
2021-09-30 17:20:18 +0200

    Initial commit

---
+++ extractor.go
@@ -0,0 +1,147 @@
+package extractor
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/service"
+	"github.com/tendermint/tendermint/types"
+)
+
+const (
+	subscriber = "ExtractorService"
+)
+
+type ExtractorService struct {
+	service.BaseService
+
+	eventBus *types.EventBus
+	handle   io.Closer
+}
+
+func NewExtractorService(eventBus *types.EventBus) *ExtractorService {
+	return &ExtractorService{eventBus: eventBus}
+}
+
+func (ex *ExtractorService) OnStart() error {
+
+	file, _ := os.Create("log-" + time.Now().Format("2006-01-02_15:04:05") + ".log")
+	ex.handle = file
+
+	sync := &sync.Mutex{}
+
+	blockSub, err := ex.eventBus.SubscribeUnbuffered(context.Background(), subscriber, types.EventQueryNewBlock)
+	if err != nil {
+		return err
+	}
+
+	txsSub, err := ex.eventBus.SubscribeUnbuffered(context.Background(), subscriber, types.EventQueryTx)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			msg := <-blockSub.Out()
+			eventData := msg.Data().(types.EventDataNewBlock)
+			height := eventData.Block.Header.Height
+
+			if err := IndexBlock(file, sync, eventData); err != nil {
+				ex.Logger.Error("failed to index block", "height", height, "err", err)
+			} else {
+				ex.Logger.Info("indexed block", "height", height)
+			}
+
+			for i := int64(0); i < int64(len(eventData.Block.Txs)); i++ {
+				msg2 := <-txsSub.Out()
+				txr := msg2.Data().(types.EventDataTx).TxResult
+				if err = IndexTx(file, sync, &txr); err != nil {
+					ex.Logger.Error("failed to index block txs", "height", height, "err", err)
+				} else {
+					ex.Logger.Debug("indexed block txs", "height", height) // , "num_txs", eventData.NumTxs)
+				}
+			}
+
+		}
+	}()
+	return nil
+}
+
+func (ex *ExtractorService) OnStop() {
+	if ex.eventBus.IsRunning() {
+		_ = ex.eventBus.UnsubscribeAll(context.Background(), subscriber)
+	}
+
+	if ex.handle != nil {
+		ex.handle.Close()
+	}
+}
+
+func IndexTx(out io.Writer, sync *sync.Mutex, result *abci.TxResult) error {
+	rawBytes, err := proto.Marshal(result)
+	if err != nil {
+		return err
+	}
+	sync.Lock()
+	io.WriteString(out, fmt.Sprintf("DMLOG TX %d %d ", result.Height, result.Index))
+	io.WriteString(out, base64.StdEncoding.EncodeToString(rawBytes))
+	io.WriteString(out, "\n")
+	sync.Unlock()
+	return err
+}
+
+func IndexBlock(out io.Writer, sync *sync.Mutex, bh types.EventDataNewBlock) error {
+	//headerB, err := bh.Block.Header.ToProto().Marshal()
+
+	blockP, err := bh.Block.ToProto()
+	if err != nil {
+		return err
+	}
+	marshaledBlock, err := blockP.Marshal()
+	if err != nil {
+		return err
+	}
+
+	sync.Lock()
+	defer sync.Unlock()
+
+	//io.WriteString(out, fmt.Sprintf("DMLOG BLOCK_HEADER %d %d ", bh.Block.Header.Height, bh.Block.Header.Time.UnixMilli()))
+	//io.WriteString(out, base64.StdEncoding.EncodeToString(headerB))
+	//io.WriteString(out, "\n")
+
+	io.WriteString(out, fmt.Sprintf("DMLOG BLOCK %d %d ", bh.Block.Header.Height, bh.Block.Header.Time.UnixMilli()))
+	io.WriteString(out, base64.StdEncoding.EncodeToString(marshaledBlock))
+	io.WriteString(out, "\n")
+
+	for i, ev := range bh.ResultBeginBlock.Events {
+		b := strings.Builder{}
+		for _, at := range ev.Attributes {
+			b.WriteString("@@")
+			b.Write(at.Key)
+			b.WriteString(":")
+			b.Write(at.Value)
+		}
+		io.WriteString(out, fmt.Sprintf("DMLOG BLOCK_BEGIN_EVENT %d %d %s %s \n", bh.Block.Header.Height, i, ev.Type, b.String()))
+	}
+
+	for i, ev := range bh.ResultEndBlock.Events {
+		b := strings.Builder{}
+		for _, at := range ev.Attributes {
+			b.WriteString("@@")
+			b.Write(at.Key)
+			b.WriteString(":")
+			b.Write(at.Value)
+		}
+		io.WriteString(out, fmt.Sprintf("DMLOG BLOCK_END_EVENT %d %d %s %s \n", bh.Block.Header.Height, i, ev.Type, b.String()))
+	}
+
+	return err
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/figment-networks/extractor-tendermint
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.4.3
+	github.com/tendermint/tendermint v0.34.9
+)
+
+require (
+	github.com/btcsuite/btcd v0.21.0-beta // indirect
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20201119123407-9b1e624d6bc4 // indirect
+	google.golang.org/grpc v1.35.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
